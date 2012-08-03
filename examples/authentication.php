f326df1cc15cf0d15b41c067ff4404b48d610b4c
Josh Holat <jholat@gmail.com>
2012-08-03 15:49:02 -0500

    expand upon request.php, add information to them

--- examples/authentication.php
+++ examples/authentication.php
@@ -4,7 +4,7 @@ require_once '../StageBloc.php';
 
 $clientId = '<YOUR CLIENT ID>';
 $clientSecret = '<YOUR CLIENT SECRET>';
-$redirectUri = '<YOUR REDIRECT URI>';
+$redirectUri = '<YOUR REDIRECT URI>'; // For use with this example, it may make sense to use some sort of localhost here
 
 $stagebloc = new Services_StageBloc($clientId, $clientSecret, $redirectUri);
 
@@ -12,8 +12,9 @@ if ( isset($_GET['code']) )
 {
 	try
 	{
-	    $accessToken = $stagebloc->accessToken($_GET['code']);
-		var_dump($accessToken);
+	    $accessToken = $stagebloc->accessToken($_GET['code']);	
+		echo 'Successfully authenticated! Your access token is ' . $accessToken['access_token'] . '<br/>' .
+				'<a href="request.php?accessToken=' . $accessToken['access_token'] . '">Try an example request</a>';
 	}
 	catch (Services_StageBloc_Invalid_Http_Response_Code_Exception $e)
 	{
@@ -23,8 +24,6 @@ if ( isset($_GET['code']) )
 else
 {
 	$authUrl = $stagebloc->getAuthorizeUrl(array('scope' => 'non-expiring'));
-?>
-	<a href="<?php echo $authUrl; ?>">Connect With StageBloc</a>
-<?php
+	echo '<a href="' . $authUrl . '">Connect With StageBloc</a>';
 }
 ?>
--- examples/request.php
+++ examples/request.php
@@ -5,13 +5,21 @@ require_once '../StageBloc.php';
 $clientId = '<YOUR CLIENT ID>';
 $clientSecret = '<YOUR CLIENT SECRET>';
 $redirectUri = '<YOUR REDIRECT URI>';
+$accessToken = '<YOUR ACCESS TOKEN>'; // This can be received using authentication.php
+
+// If sent here from authentication.php, we can grab the access token from there
+if ( ! empty($_GET) && isset($_GET['accessToken']) )
+{
+	$accessToken = $_GET['accessToken'];
+}
 
 $stagebloc = new Services_StageBloc($clientId, $clientSecret, $redirectUri);
-$stagebloc->setAccessToken('bd13f86dd13e1c5be2b439a3bd36a37a0238f1e0');
-$stagebloc->setResponseFormat('xml'); // XML is default, JSON is also accepted
+$stagebloc->setAccessToken($accessToken);
+$stagebloc->setResponseFormat('json'); // XML is default, JSON is also accepted
 
-$tracks = $stagebloc->get('audio/list');
+$items = $stagebloc->get('statuses/list');
 
-var_dump($tracks);
+// If the response format is XML, it might be easier to understand the output by viewing the page source
+echo $items;
 
 ?>
